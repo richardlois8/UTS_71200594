2c368032a7131f2812d4e68b68bd31d593d29308
Richard Lois <78334592+richardlois8@users.noreply.github.com>
2022-04-04 15:20:03 +0700

    Update class Vehicle

---
+++ Vehicle/src/main/java/com/uts/soal1/Vehicle.java
@@ -0,0 +1,9 @@
+package com.uts.soal1;
+
+public interface Vehicle {
+    public void backward();
+    public void brake();
+    public void forward();
+    public void turnLeft();
+    public void turnRight();
+}
